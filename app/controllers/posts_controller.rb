66d5cdcba207acc2f70dac700ab4cae2b118f8f8
Chely Ho <adept_inept@yahoo.com>
2018-04-07 20:56:36 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
 
 	private
 
-	def post_params
-	  params.require(:post).permit(:title, :description)
+	def post_params(*args)
+  params.require(:post).permit(*args)
 	end
 end
